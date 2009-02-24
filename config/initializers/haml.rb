34cd71bc48a8009f587e7646e2dd59abf4d05012
Patrick Crowley <patrick@mokolabs.com>
2009-02-23 19:52:28 -0800

    updated haml config to support sass, simplified haml template names, added homepage controller and view

--- config/initializers/haml.rb
+++ config/initializers/haml.rb
@@ -1,1 +1,4 @@
 Haml::Template.options = { :attr_wrapper => '"' }
+Sass::Plugin.options = { :template_location => "#{RAILS_ROOT}/public/sass", 
+                         :css_location => "#{RAILS_ROOT}/public/stylesheets" }
+Sass::Plugin.update_stylesheets
