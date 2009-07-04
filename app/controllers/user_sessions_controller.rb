0b8679d6ac4ab1f91f0f1c3e2d330a4ba9c3cd4d
Neal Clark <nclark@thrownproject.org>
2009-07-03 23:08:56 -0700

    refactoring user_sessions_controller.rb

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,6 +1,5 @@
 class UserSessionsController < ApplicationController
   before_filter :require_not_logged_in, :only => [:new, :create]
-  before_filter :require_login, :only => :destroy
 
   def new
     @user_session = UserSession.new
@@ -17,19 +16,16 @@ class UserSessionsController < ApplicationController
   end
 
   def destroy
-    current_user_session.destroy
-    flash[:notice] = "Logout successful!"
+    if current_user.nil?
+      flash[:notice] = "You're already logged out!"
+    else
+      current_user_session.destroy
+      flash[:notice] = "Logout successful!"
+    end
     redirect_to root_path
   end
 
   private
-  def require_login
-    if current_user.nil?
-      flash[:notice] = "You're already logged out!"
-      redirect_to root_path
-    end
-  end
-
   def require_not_logged_in
     if current_user
       redirect_to account_path
