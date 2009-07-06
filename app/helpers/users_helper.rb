47b40c66533c364058cf133b04ddd86c02aff8a8
Patrick Crowley <patrick@mokolabs.com>
2009-07-05 19:54:50 -0700

    updated linkify helper to show icons for popular web apps

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,19 @@
 module UsersHelper
   
-  def linkify(str, separator = "\n")
-    str.split(separator).collect {|link| link_to link, link}.join '<br />'
+  def iconify_and_link(links, options={})
+    options[:separator] = "\n" if options[:separator].blank?
+    links.split(options[:separator]).collect do |link|
+      icon = String.new
+      ['github','twitter','flickr','delicious','aim', 'digg', 'yelp', 'vimeo'].each do |service|
+        if link.match(/#{service}/)
+          icon += link_to(image_tag("application/social/#{service}_#{options[:size]}.png"), link)
+        end
+      end
+      if icon.blank?
+        icon += link_to(image_tag("application/social/web_#{options[:size]}.png"), link)
+      end
+      icon + " " + link_to(link, link)
+    end.join '<br />'
   end
   
   def show_github_projects_for(user)
