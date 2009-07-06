100f2f91b7cbb64de5759c280960123b444a7099
Patrick Crowley <patrick@mokolabs.com>
2009-07-06 11:26:10 -0700

    strip line breaks from member links

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,6 +3,7 @@ module UsersHelper
   def iconify_and_link(links, options={})
     options[:separator] = "\n" if options[:separator].blank?
     links.split(options[:separator]).collect do |link|
+      link = link.strip
       icon = String.new
       WEBSITES.each do |website|
         if link.match(/#{website}/) && icon.blank?
@@ -12,8 +13,8 @@ module UsersHelper
       if icon.blank?
         icon += link_to(image_tag("application/social/web_#{options[:size]}.png"), link)
       end
-      "<li>" + icon + " " + link_to(link, link) + "</li>"
-    end.join "\n"
+      "<li>" + icon + " " + link_to(link, link) + "</li>\n"
+    end
   end
   
   def show_github_projects_for(user)
