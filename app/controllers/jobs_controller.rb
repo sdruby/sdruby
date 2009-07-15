71bd7f568142ccc668cda6c13ab4f21110ac91a0
Patrick Crowley <patrick@mokolabs.com>
2009-07-15 11:35:47 -0700

    removed XML support from jobs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,29 +4,14 @@ class JobsController < ApplicationController
 
   def index
     @jobs = Job.published.newest_first
-
-    respond_to do |format|
-      format.html
-      format.xml  { render :xml => @jobs }
-    end
   end
 
   def show
     @job = Job.find(params[:id])
-
-    respond_to do |format|
-      format.html
-      format.xml  { render :xml => @job }
-    end
   end
 
   def new
     @job = Job.new
-
-    respond_to do |format|
-      format.html
-      format.xml  { render :xml => @job }
-    end
   end
 
   def edit
@@ -36,41 +21,31 @@ class JobsController < ApplicationController
   def create
     @job = current_user.jobs.build(params[:job])
 
-    respond_to do |format|
-      if @job.save
-        flash[:notice] = 'Job was successfully created.'
-        format.html { redirect_to(@job) }
-        format.xml  { render :xml => @job, :status => :created, :location => @job }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @job.errors, :status => :unprocessable_entity }
-      end
+    if @job.save
+      flash[:notice] = 'Job was successfully created.'
+      redirect_to(@job)
+    else
+      render :action => "new"
     end
   end
 
   def update
-    respond_to do |format|
-      if @job.update_attributes(params[:job])
-        flash[:notice] = 'Job was successfully updated.'
-        format.html { redirect_to(@job) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @job.errors, :status => :unprocessable_entity }
-      end
+    if @job.update_attributes(params[:job])
+      flash[:notice] = 'Job was successfully updated.'
+      redirect_to(@job)
+    else
+      render :action => "edit"
     end
   end
 
   def destroy
     @job.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(jobs_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(jobs_url)
   end
 
   private
+  
   def require_login
     if current_user.nil?
       flash[:notice] = "Please login to post a job "
@@ -85,4 +60,5 @@ class JobsController < ApplicationController
       redirect_to jobs_path
     end
   end
+
 end
