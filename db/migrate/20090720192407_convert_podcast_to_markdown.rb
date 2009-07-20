d4f2e9752bfcd76a7c9435c03f6d59040a1339de
Patrick Crowley <patrick@mokolabs.com>
2009-07-20 13:55:29 -0700

    tweaked textile to markdown method

--- db/migrate/20090720192407_convert_podcast_to_markdown.rb
+++ db/migrate/20090720192407_convert_podcast_to_markdown.rb
@@ -2,7 +2,7 @@ class ConvertPodcastToMarkdown < ActiveRecord::Migration
   # Convert Textile links to Markdown links
   def self.up
     Podcast.find(:all).each do |podcast|
-      podcast.description = podcast.description.gsub(/\"([\w ]*)\"\:(\S+)/, ('[\1](\2)'))
+      podcast.description = podcast.description.gsub(/\"([^\"]*)\"\:([^[:space:],]*)/, ('[\1](\2)'))
       podcast.save
     end
   end
