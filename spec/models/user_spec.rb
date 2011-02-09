900316d8113c1f797ef2d23baba16a2890255c61
Patrick Crowley <patrick@mokolabs.com>
2011-02-08 18:29:03 -0800

    removed old jobs stuff from user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,6 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe User do
-  it { should have_many(:jobs) }
   it { should have_many(:projects) }
 
   before { @user = Factory(:user, :full_name => "Snoop Dog") }
