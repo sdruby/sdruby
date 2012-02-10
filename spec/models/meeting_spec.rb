6cf9428c7326c013f2aea2711262af00634e45dd
Rob Kaufman <rob@notch8.com>
2012-02-09 21:15:34 -0800

    there is no meeting model right now

--- spec/models/meeting_spec.rb
+++
@@ -1,9 +0,0 @@
-require File.dirname(__FILE__) + '/../spec_helper'
-
-describe "Meeting" do
-  before { @meeting = Factory(:meeting) }
-
-  it "should be valid" do
-    @meeting.should be_valid
-  end
-end
