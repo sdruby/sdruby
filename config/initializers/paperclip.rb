5df1e69cee4ce3769937aaac1a0f68554c80de53
James Miller <bensie@gmail.com>
2012-08-21 15:07:33 -0700

    Heroku has imagemagick commands included in the PATH

--- config/initializers/paperclip.rb
+++
@@ -1,2 +0,0 @@
-# Joyent doesn't put "convert" or "identify", required by Paperclip, in the PATH.  We need to instruct it:
-Paperclip.options[:image_magick_path] = "/opt/local/bin/" if Rails.env.production?
