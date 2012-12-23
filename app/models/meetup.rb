24e024a1b26386c10d6921884a2565cb8c133970
Kevin Thompson <kevin@kevinthompson.info>
2012-12-23 12:18:45 -0800

    Use venue name and address from Meetup.com

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -17,6 +17,35 @@ class Meetup
     events.first
   end
 
+  def date
+    next_event.time.strftime("%A, %B %e")
+  end
+
+  def time
+    next_event.time.strftime("%l:%M%P,")
+  end
+
+  def venue
+    next_event.venue_name
+  end
+
+  def address
+    @address ||= begin
+      address = ''
+      address << [next_event.venue_address1, next_event.venue_address2, next_event.venue_city, next_event.venue_state].reject(&:blank?).join(',')
+      address << " #{next_event.venue_zip}"
+      address
+    end
+  end
+
+  def map_url
+    if venue =~ /U(niversity of )?C(alifornia )?S(an )?D(iego)?/i
+      'http://tinyurl.com/2f486e'
+    else
+      "https://maps.google.com/maps?daddr=#{URI.escape(address)}"
+    end
+  end
+
 private
 
   def client
